d8cf3559e052c10bd4138d5498c7dab504def500
tianshufoc <90360111+tianshufoc@users.noreply.github.com>
2021-09-08 20:13:32 -0400

    Add files via upload

---
+++ Ricitation1.py
@@ -0,0 +1,15 @@
+x=10
+print(x)
+print(x-11)
+my_string='hi, how are you'
+print(my_string)
+
+points_obtained=99
+total_points=100
+precentage=(points_obtained/total_points)*100
+print(precentage)
+
+x="I am a UB student8"
+print(x[0])
+print(x[5])
+print(x[-1])
